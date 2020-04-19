a61ad479d5cd8fc3c23af42fbdd5ffc40475c153
宋彦杉 <songyanshan.2008@163.com>
2020-04-19 18:12:40 -0400

    fix buges

---
+++ code/gis.py
@@ -0,0 +1,70 @@
+import folium
+
+from preprocess import PreProcess
+
+from utils import *
+
+from folium import FeatureGroup, LayerControl, Map, GeoJson, GeoJsonTooltip
+
+from folium.plugins import FeatureGroupSubGroup
+
+
+class GIS:
+    def __init__(self):
+        self.preprocess = PreProcess()
+
+    def draw_CDBG(self):
+        geo_map = Map(location=[42.795390191429625, -71.07516023514027], zoom_start=12)
+        data = self.preprocess.get_CDBG_geometry_data()
+        geo_json = GeoJson(
+            data,
+            style_function=lambda feature: {
+                'fillColor': '#ffff00',
+                'color': 'black',
+                'weight': 1,
+                # 'dashArray': '1, 1'
+            }
+        )
+        geo_json.add_to(geo_map)
+        geo_map.save('release/CDBG.html')
+
+    def draw_precincts_wards(self):
+        geo_map = Map(location=[42.795390191429625, -71.07516023514027], zoom_start=12)
+        data = self.preprocess.get_precincts_wards_geometry_data()
+        for i in range(len(data)):
+            ward_info = data[i]
+            ward_number = ward_info['ward']
+            feature_group = FeatureGroup(name='Ward' + ward_number, show=True)
+            precincts = ward_info['data']
+            for j in range(len(precincts)):
+                precinct = precincts[j]
+                sub_feature_group = FeatureGroupSubGroup(name='Precinct'+precinct['properties']['Precinct'], group=feature_group, control=True, show=True)
+                geo_json = GeoJson(
+                    data=precinct,
+                    style_function=lambda feature: {
+                        'fillColor': '#00BFFF',
+                        'color': 'black',
+                        'weight': 1,
+                    },
+                    highlight_function=lambda feature: {
+                        'fillColor': '#FFBFFF',
+                        'color': 'yellow',
+                        'weight': 2,
+                    },
+                    tooltip=GeoJsonTooltip(fields=['Precinct']),
+                    overlay=True
+                )
+                geo_json.add_to(sub_feature_group)
+                sub_feature_group.add_to(feature_group)
+            feature_group.add_to(geo_map)
+        LayerControl().add_to(geo_map)
+        geo_map.save('release/precincts_wards.html')
+
+    def draw_division(self):
+        self.draw_CDBG()
+        self.draw_precincts_wards()
+
+
+if __name__ == '__main__':
+    gis = GIS()
+    gis.draw_division()
---
+++ code/utils.py
@@ -0,0 +1,14 @@
+import pyproj
+
+def epsg3857_to_epsg4326(x: float, y:float) -> list:
+    p1 = pyproj.Proj("epsg:3857")
+    p2 = pyproj.Proj("epsg:4236")
+
+    return pyproj.transform(p1, p2, x, y)
+
+
+if __name__ == '__main__':
+    x = -7913112.7619000003
+    y = 5280261.8451000005
+    res = epsg3857_to_epsg4326(x, y)
+    print(res)
